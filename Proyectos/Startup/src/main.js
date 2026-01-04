daf3976d6c095665a5f7f0dd941056c2e4f4a97c
jesur <juansurianodev@gmail.com>
2026-01-03 20:59:49 -0600

    Dando estilos a la seccion de caracteristicas

--- Proyectos/Startup/src/main.js
+++ Proyectos/Startup/src/main.js
@@ -1,5 +1,6 @@
-import '../src/css/normalize.css';
-import '../src/css/variables.css';
-import '../src/css/estilosStartup.css';
-import '../src/css/header.css';
-import '../src/css/hero.css';
+import "../src/css/normalize.css";
+import "../src/css/variables.css";
+import "../src/css/estilosStartup.css";
+import "../src/css/header.css";
+import "../src/css/hero.css";
+import "../src/css/caracteristicas.css";
